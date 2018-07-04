0a8cda8bd56dd7ea30955ace313aa9beaf41c6f1
BinZhu <30952106+binzh93@users.noreply.github.com>
2018-07-05 00:06:13 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,5 +7,6 @@ import caffe
 caffe.set_device(0)
 caffe.set_mode_gpu()
 solver = caffe.SGDSolver('solver.prototxt')
-solver.net.copy_from('/workspace/mnt/group/face-det/zhubin/train_file/initial.caffemodel')
+#solver.net.copy_from('/workspace/mnt/group/face-det/zhubin/train_file/initial.caffemodel')
 solver.solve()
+
