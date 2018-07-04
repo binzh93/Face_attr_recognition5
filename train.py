9d405a1c127a66e88ef657acac74878e2d8b15f6
BinZhu <30952106+binzh93@users.noreply.github.com>
2018-07-04 10:19:42 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.append('/workspace/mnt/group/face-det/zhubin/caffe/python')
 import caffe
-caffe.set_device(1)
+caffe.set_device(0)
 caffe.set_mode_gpu()
-solver = caffe.SGDSolver('/workspace/mnt/group/face-det/zhubin/Face_attr_recognition5/solver.prototxt')
+solver = caffe.SGDSolver('solver.prototxt')
+solver.net.copy_from('/workspace/mnt/group/face-det/zhubin/train_file/initial.caffemodel')
 solver.solve()
