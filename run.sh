103a30cb6a1117e6cc3643cf309a762a7dc5a371
BinZhu <30952106+binzh93@users.noreply.github.com>
2018-07-05 00:11:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd /workspace/mnt/group/face-det/zhubin/Face_attr_recognition5/
-echo '===>Start training!' #>> GenderAge.log
-python train.py
-echo '===>Training finished!' #>> GenderAge.log
+echo '===>Start training!' >> GenderAge.log
+#python train.py
+nohup python train.py > GenderAge.log 2>&1 &
+echo '===>Training finished!' >> GenderAge.log
